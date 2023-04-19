b484b0a846b57ff1affc29da737e4972017f448a
AlexChavezz <alexischavez1234.ac@gmail.com>
2023-04-18 18:16:26 -0600

    fetch role string

--- src/entities/User.py
+++ src/entities/User.py
@@ -1,11 +1,12 @@
 from werkzeug.security import check_password_hash
 from flask_login import UserMixin
 class User(UserMixin):
-    def __init__(self, user_id,  username, id_role, password) -> None:
+    def __init__(self, user_id,  username, id_role, password, role) -> None:
         self.id = user_id
         self.username = username
         self.id_role = id_role
         self.password = password
+        self.role = role
     @classmethod
     def check_password(self, hased_password, password):
         return check_password_hash(hased_password, password)
